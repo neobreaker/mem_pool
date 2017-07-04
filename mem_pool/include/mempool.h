aa2b90fa9fa49481e21dbfdf58556fa077156ba8
Neo <769104314@qq.com>
2017-07-04 11:16:32 +0800

    extended tbl size

--- mem_pool/include/mempool.h
+++ mem_pool/include/mempool.h
@@ -38,32 +38,57 @@ typedef __I uint8_t vuc8;   /*!< Read Only */
 
 typedef enum
 {
-	mem_err_OK,
-	mem_err_EMPTY,
-	mem_err_NULL,
-	mem_err_OUT_OF_RANGE,
-}mem_err_t;
+    mem_err_OK,
+    mem_err_EMPTY,
+    mem_err_NULL,
+    mem_err_OUT_OF_RANGE,
+} mem_err_t;
 
+typedef enum
+{
+	TBL_W8  = 1,
+	TBL_W16 = 2,
+	TBL_W32 = 4,
+}tbl_width_t;
+
+typedef struct
+{
+    u8  *tbl_w8;
+	u16 *tbl_w16;
+	u32 *tbl_w32;
+} mem_tbl_t;
 
 //内存管理控制器
-typedef struct 
+typedef struct
 {
-	u8  *mem_base;
-	u32  mem_base_size;
-	u8  *mem_tbl;
-	u32  mem_tbl_size;
-	u32  mem_block_size;
-	u32  mem_block_num;
-	u32  mem_valid_size;
+    u8  *mem_base;
+    u32  mem_base_bytes;
+    //u8  *mem_tbl;
+    mem_tbl_t    mem_tbl;
+	tbl_width_t  tbl_width;  
+    u32  tbl_bytes;
+	u32  tbl_block_num;
+    u32  block_bytes;
+    u32  mem_block_num;
+    u32  mem_valid_size;
+	u32  mem_free_size;
 } mem_dev_t;
 
-mem_err_t mem_pool_init(mem_dev_t* mem_dev, char* mem_base, u32 mem_base_size, u32 block_size);
-void * mem_pool_alloc(mem_dev_t* mem_dev, u32 num_byte);
+mem_err_t mem_pool_init(mem_dev_t* mem_dev, char* mem_base, u32 mem_base_bytes, u32 block_size, tbl_width_t tbl_width);
 void * mem_pool_malloc(mem_dev_t* mem_dev, u32 num_byte);
 void * mem_pool_calloc(mem_dev_t* mem_dev, u32 n, u32 size);
 void mem_pool_memset(char *p, char c, u32 num_byte);
 void mem_pool_free(mem_dev_t *mem_dev, char *p);
 void mem_pool_print_tbl(mem_dev_t *mem_dev);
 float mem_pool_utilization(mem_dev_t *mem_dev);
+
+static void* mem_pool_alloc(mem_dev_t* mem_dev, u32 num_byte);
+static void* mem_set_tbl_8(mem_dev_t *mem_dev, u32 num_block);
+static void* mem_set_tbl_16(mem_dev_t *mem_dev, u32 num_block);
+static void* mem_set_tbl_32(mem_dev_t *mem_dev, u32 num_block);
+static void  mem_reset_tbl_8(mem_dev_t *mem_dev, char *p);
+static void  mem_reset_tbl_16(mem_dev_t *mem_dev, char *p);
+static void  mem_reset_tbl_32(mem_dev_t *mem_dev, char *p);
+
 #endif
 
